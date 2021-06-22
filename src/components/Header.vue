87f635042a865eb8750976e3ad6da3d7c5180396
Nhikos <4ndre.sergi@gmail.com>
2021-06-22 17:19:08 +0200

    working-main

---
+++ src/components/Header.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="header">
+   <div class="nav">
+       <button> order online </button>
+       <div class="actions">
+        <img class="svg" src="../assets/svg/svg-0.svg" alt=""><a href="#">lorem</a>
+        <a href="#">lorem</a>
+        <a href="#">lorem</a>
+        <img src="../assets/img/h5-logo-divided-header.png" alt="">
+        <a href="#">lorem</a>
+        <a href="#">lorem</a>
+        <a href="#">lorem</a>
+       </div>
+       <div class="shop-action">
+        <img class="svg" src="../assets/svg/svg-1.svg" alt=""><a href="#">lorem</a>
+        <img class="svg" src="../assets/svg/svg-11.svg" alt=""><a href="#">lorem</a>
+       </div>
+   </div>
+   <img src="../assets/img/h3-rev-img-6.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+.header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 422px;
+    background-image: url(../assets/img/h3-rev-img-5.png);
+    background-size: 41%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-color: black;
+}
+
+.nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 25%;
+    width: 60%;
+    border: 1px solid blue;
+}
+
+.header > img {
+    width: 16%;
+}
+
+a {
+    font-size: 12px;
+    text-decoration: none;
+    color: white;
+}
+.actions{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.actions > .svg {
+    width: 13px;
+    margin: 3px
+}
+.actions > a {
+    margin-right: 25px;
+}
+.actions > img {
+    width: 100px;
+    margin-right: 26px;
+}
+
+.shop-action {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.shop-action > img {
+    height: 20px;
+}
+.shop-action > a {
+    margin-right: 10px;
+}
+
+
+button {
+    color: white;
+    font-size: 12px;
+    padding: 10px 20px;
+    background-color: orangered;
+    text-transform: uppercase;
+    border: none;
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="main">
+      <div class="main-top">
+          <section class="main-section-top">
+            <img src="../assets/img/h3-img-1.jpg" alt="">
+            <img src="../assets/img/h3-img-2.jpg" alt="">
+            <img src="../assets/img/h3-img-3.jpg" alt="">
+            <img src="../assets/img/h3-img-4.jpg" alt="">
+          </section>
+          <section class="main-section-mid">
+              <div class="prev ball">
+                  <span>prev</span>
+              </div>
+              <div class="cit-container">
+                <i class="fas fa-quote-left"></i>
+                <h3>"FORGET THE TRENDY PIZZA SHOPS, THIS HIDDEN SPOT MAKES THE BEST NEW YORK-STYLE PIZZA SLICE IN NAPLES"</h3>
+                <span class="location">WASHINGTON POST 2018</span>
+                <div class="circles">
+                    <span class="circle"></span>
+                    <span class="circle"></span>
+                    <span class="circle"></span>
+                </div>
+              </div>
+              <div class="next ball">
+                  <span>next</span>
+              </div>
+          </section>
+          <section class="main-section-bot">
+            <section class="main-section-bot_left">
+
+            </section>
+            <section class="main-section-bot_right">
+                <div class="offers_container">
+                        <h2>SPECIALS*</h2>
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, deleniti!</span>
+                    <div class="offer-description">
+                        <div class="offer-price">
+                            <h3>$10</h3>
+                        </div>
+                        <div class="offer-type">
+                            <h3>COMBO PICCOLO</h3>
+                            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, est.</span>
+                        </div>
+                    </div>
+                    <div class="offer-description">
+                        <div class="offer-price">
+                            <h3>$20</h3>
+                        </div>
+                        <div class="offer-type">
+                            <h3>COMBO MEZZO</h3>
+                            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, est.</span>
+                        </div>
+                    </div>
+                    <div class="offer-description">
+                        <div class="offer-price">
+                            <h3>$30</h3>
+                        </div>
+                        <div class="offer-type">
+                            <h3>COMBO GRANDE</h3>
+                            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, est.</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+          </section>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Main"
+}
+</script>
+
+<style scoped>
+/* COMMON */
+.main {
+    margin-top: 6px;
+}
+/* /COMMON */
+
+/* MAIN TOP */
+/* main-SECTION-TOP */
+.main-section-top {
+    display: flex;
+    justify-content: space-between;
+}
+.main-section-top img {
+    display: flex;
+    width: calc(100% / 4 - 6px)
+}
+/* /SECTION TOP */
+
+/* SECTION MIDDLE */
+.main-section-mid {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 316px;
+    margin: 5px 0;
+    background-image: url(../assets/img/h3-testimonials-bckgrnd.jpg);
+    background-position: center;
+}
+
+.fa-quote-left {
+    font-size: 45px;
+    margin-bottom: 15px;
+}
+
+.main-section-mid h3 {
+    text-align: center;
+    margin-bottom: 10px
+}
+
+.location {
+    color: red;
+    margin-bottom: 15px;
+    font-size: 10px;
+}
+
+
+.circle {
+    display: inline-block;
+    height: 5px;
+    width: 5px;
+    padding: 3px 3px;
+    margin: 10px 3px;
+    border-radius: 50%;
+    background-color: red;
+}
+
+
+.cit-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 45%;
+    border: 1px solid blue;
+}
+
+.prev {
+    color: red;
+    writing-mode: vertical-rl;
+}
+.next {
+    color: red;
+    writing-mode: vertical-rl;
+    transform:rotateZ(180deg)
+}
+.ball {
+    padding: 18px 10px;
+    background-color: white;
+    border-top-right-radius: 50%;
+    border-bottom-right-radius: 50%;
+}
+/* /SECTION MIDDLE */
+
+/* SECTION BOTTOM */
+.main-section-bot {
+    display: flex;
+    height: 500px;
+    width: 100%;
+    border: 1px solid blue
+}
+
+/* SECTION BOTTOM-LEFT */
+.main-section-bot_left {
+    width: 50%;
+    border: 1px solid red;
+    background-image:url(../assets/img/h1-img-4.jpg);
+    background-size: cover;
+}
+/* /SECTION BOTTOM-LEFT */
+
+/* SECTION BOTTOM-RIGHT */
+.main-section-bot_right {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+}
+
+
+.offers_container {
+    height: 60%;
+    width: 50%;
+    border: 1px solid blue;
+}
+.offer-description{
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid yellow;
+}
+.offer-type {
+    width: 85%;
+    border: 1px solid greenyellow;
+}
+/* /SECTION BOTTOM-RIGHT */
+
+/* /SECTION BOTTOM */
+</style>
